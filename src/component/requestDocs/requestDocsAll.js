5ba2956c27b236367462d67b9e81cc95840d9d25
Attawit Chaiyaroj <ahm.913@hotmail.com>
2018-04-12 15:38:54 +0700

    Create request docs page

---
+++ src/component/requestDocs/requestDocsAll.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Table, Button } from 'react-bootstrap'
+
+import {MOCK_SEMS, MOCK_SUBJECTS} from './mockData.json'
+
+export default class RequestDocsAll extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+					
+        }
+    }
+
+    render(){
+        return(
+                <div>
+									
+                </div>
+        )
+    }
+}

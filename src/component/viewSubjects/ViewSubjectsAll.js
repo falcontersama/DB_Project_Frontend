fec5485e59c49d87babf08b42ddb52829bda6d32
Attawit Chaiyaroj <ahm.913@hotmail.com>
2018-04-13 14:55:05 +0700

    Moved function onWithdrawButton into class

--- src/component/viewSubjects/ViewSubjectsAll.js
+++ src/component/viewSubjects/ViewSubjectsAll.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Table, Button, MenuItem, DropdownButton } from 'react-bootstrap'
 import styled from 'styled-components'
 
-import {MOCK_SUBJECTS} from './MockData.json'
+import { MOCK_SUBJECTS } from './MockData.json'
 
 const ViewSubjectBox = styled.div`
     border:1px solid lightgrey;
@@ -16,30 +16,33 @@ const ViewSubjectTable = styled.div`
 `;
 
 const withdrawButtonText = 'ถอน'
-function onWithdrawButton(subject){
 
-}
-
-function getSemester(subject){
+function getSemester(subject) {
     return subject.year + '/' + subject.semester;
 }
 
-export default class ViewSubjectsAll extends Component{
-    constructor(props){
+export default class ViewSubjectsAll extends Component {
+    constructor(props) {
         super(props)
         var subjects = MOCK_SUBJECTS
         var sems = [... new Set(subjects.map((obj, idx) => getSemester(obj)))].sort((a, b) => (a == b ? 0 : a < b ? 1 : -1))
         var latestSem = sems[0]
         this.state = {
-            "semesters" : sems,
-            "subjects" : subjects,
-            "currSem" : latestSem,
-            "latestSem" : latestSem
+            "semesters": sems,
+            "subjects": subjects,
+            "currSem": latestSem,
+            "latestSem": latestSem
         }
+
+        this.onWithdrawButton = this.onWithdrawButton.bind(this)
     }
 
-    render(){
-        return(
+    onWithdrawButton(subject) {  
+
+    }
+
+    render() {
+        return (
             <div>
                 <h1>{this.props.nameLog} {this.props.usernameLog}</h1>
                 <ViewSubjectBox>
@@ -47,7 +50,7 @@ export default class ViewSubjectsAll extends Component{
                         {this.state.semesters.map((obj, index) => (
                             <MenuItem
                                 eventKey={index}
-                                onClick={() => {this.setState({"currSem" : obj})}}
+                                onClick={() => { this.setState({ "currSem": obj }) }}
                             >
                                 {obj}
                             </MenuItem>
@@ -63,32 +66,32 @@ export default class ViewSubjectsAll extends Component{
                                     <th>สถานที่</th>
                                     <th>อาจารย์ผู้สอน</th>
                                     <th>ผลการเรียน</th>
-                                    <th/>
+                                    <th />
                                 </tr>
                             </thead>
                             <tbody>
                                 {this.state.subjects
-                                .filter((item) => (getSemester(item) == this.state.currSem))
-                                .map((obj, index) => (
-                                    <tr>
-                                        <td>{obj.subjectID} {obj.subjectName}</td>
-                                        <td>{obj.section}</td>
-                                        <td>{obj.classes.map((x, idx) => <span key={idx}>{x.day} {x.time}<br/></span>)}</td>
-                                        <td>{obj.classes.map((x, idx) => <span key={idx}>{x.roomID} {x.buildingID}<br/></span>)}</td>
-                                        <td>{obj.teacher}</td>
-                                        <td>{obj.grade}</td>
-                                        <td>
-                                            <Button
-                                                bsStyle='danger'
-                                                bsSize='xsmall'
-                                                onClick={onWithdrawButton}
-                                                disabled={this.state.currSem != this.state.latestSem}
-                                            >
-                                                {withdrawButtonText}
-                                            </Button>
-                                        </td>
-                                    </tr>
-                                ))}
+                                    .filter((item) => (getSemester(item) == this.state.currSem))
+                                    .map((obj, index) => (
+                                        <tr>
+                                            <td>{obj.subjectID} {obj.subjectName}</td>
+                                            <td>{obj.section}</td>
+                                            <td>{obj.classes.map((x, idx) => <span key={idx}>{x.day} {x.time}<br /></span>)}</td>
+                                            <td>{obj.classes.map((x, idx) => <span key={idx}>{x.roomID} {x.buildingID}<br /></span>)}</td>
+                                            <td>{obj.teacher}</td>
+                                            <td>{obj.grade}</td>
+                                            <td>
+                                                <Button
+                                                    bsStyle='danger'
+                                                    bsSize='xsmall'
+                                                    onClick={this.onWithdrawButton}
+                                                    disabled={this.state.currSem != this.state.latestSem}
+                                                >
+                                                    {withdrawButtonText}
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    ))}
                             </tbody>
                         </Table>
                     </ViewSubjectTable>

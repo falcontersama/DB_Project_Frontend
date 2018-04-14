72cb9f79a0c8652da8d277c58d6eef09c4fd83d0
Attawit Chaiyaroj <ahm.913@hotmail.com>
2018-04-14 17:10:13 +0700

    Load from API

--- src/component/viewSubjects/ViewSubjectsAll.js
+++ src/component/viewSubjects/ViewSubjectsAll.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import { Table, Button, MenuItem, DropdownButton } from 'react-bootstrap'
 import styled from 'styled-components'
+import axios from 'axios'
 
 import { MOCK_SUBJECTS } from './MockData.json'
+
+const API_URL = 'http://localhost:3002/studentCourse'
 
 const ViewSubjectBox = styled.div`
     border:1px solid lightgrey;
@@ -24,17 +27,34 @@ function getSemester(subject) {
 export default class ViewSubjectsAll extends Component {
     constructor(props) {
         super(props)
-        var subjects = MOCK_SUBJECTS
-        var sems = [... new Set(subjects.map((obj, idx) => getSemester(obj)))].sort((a, b) => (a == b ? 0 : a < b ? 1 : -1))
-        var latestSem = sems[0]
+        // var subjects = MOCK_SUBJECTS
+        var username = '0123456789'
+        // var username = this.props.usernameLog
+        var subjects = []
+        axios.get(API_URL, {params: {studentID: username}})
+            .then((response) => this.loadSubjects(response.data.data))
+            .catch((error) => console.log(error))
         this.state = {
+            semesters: [],
+            subjects: [],
+            currSem: '',
+            latestSem: ''
+        }
+
+        this.onWithdrawButton = this.onWithdrawButton.bind(this)
+        this.loadSubjects = this.loadSubjects.bind(this)
+    }
+
+    loadSubjects(subjects) {
+        // console.log(subjects)
+        var sems = [...new Set(subjects.map((obj, idx) => getSemester(obj)))].sort((a, b) => (a === b ? 0 : a < b ? 1 : -1))
+        var latestSem = sems[0]
+        this.setState({
             semesters: sems,
             subjects: subjects,
             currSem: latestSem,
             latestSem: latestSem
-        }
-
-        this.onWithdrawButton = this.onWithdrawButton.bind(this)
+        })
     }
 
     onWithdrawButton(subject) {  
@@ -49,7 +69,7 @@ export default class ViewSubjectsAll extends Component {
             <div>
                 <h1>{this.props.nameLog} {this.props.usernameLog}</h1>
                 <ViewSubjectBox>
-                    <DropdownButton title={this.state.currSem}>
+                    <DropdownButton title={this.state.currSem} id='semesterDropdown'>
                         {this.state.semesters.map((obj, index) => (
                             <MenuItem
                                 eventKey={index}
@@ -74,21 +94,21 @@ export default class ViewSubjectsAll extends Component {
                             </thead>
                             <tbody>
                                 {this.state.subjects
-                                    .filter((item) => (getSemester(item) == this.state.currSem))
+                                    .filter((item) => (getSemester(item) === this.state.currSem))
                                     .map((obj, index) => (
                                         <tr>
                                             <td>{obj.subjectID} {obj.subjectName}</td>
-                                            <td>{obj.section}</td>
-                                            <td>{obj.classes.map((x, idx) => <span key={idx}>{x.day} {x.time}<br /></span>)}</td>
-                                            <td>{obj.classes.map((x, idx) => <span key={idx}>{x.roomID} {x.buildingID}<br /></span>)}</td>
-                                            <td>{obj.teacher}</td>
+                                            <td>{obj.detail.sec}</td>
+                                            <td>{obj.detail.time.map((x, idx) => <span key={idx}>{x.day} {x.time}<br /></span>)}</td>
+                                            <td>{obj.detail.time.map((x, idx) => <span key={idx}>{x.roomID} {x.buildingID}<br /></span>)}</td>
+                                            <td>{obj.detail.teacher.map((x, idx) => <span key={idx}>{x}<br /></span>)}</td>
                                             <td>{obj.grade}</td>
                                             <td>
                                                 <Button
                                                     bsStyle='danger'
                                                     bsSize='xsmall'
                                                     onClick={() => this.onWithdrawButton(obj)}
-                                                    disabled={this.state.currSem != this.state.latestSem}
+                                                    disabled={this.state.currSem !== this.state.latestSem}
                                                 >
                                                     {withdrawButtonText}
                                                 </Button>

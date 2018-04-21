d3efc46872ba03eac29fb109287871c3da3836cf
Attawit Chaiyaroj <ahm.913@hotmail.com>
2018-04-22 00:26:11 +0700

    Add Assignment slightly more usable

--- src/component/teacherAddAssignment/TeacherAddAssignmentAll.js
+++ src/component/teacherAddAssignment/TeacherAddAssignmentAll.js
@@ -10,26 +10,48 @@ import { FormGroup, FormControl, Col, Row, Button } from 'react-bootstrap'
 export default class ViewSubjects extends Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			subject: '',
+			dueDate: '',
+			description: ''
+		}
+		this.submitProcess = this.submitProcess.bind(this)
+	}
+
+	submitProcess() {
+		window.alert("TODO: Submit\nSubject: " + this.state.subject + "\nDue Date: " + this.state.dueDate + "\nDescription: " + this.state.description)
 	}
 
 	render() {
 		return (
 			<div>
 				<form horizontal>
-					<FormGroup controlId="formAddAssignmentHead">
+					<FormGroup>
 						<Col xs={3}>
-							<FormControl controlId="formAddAssignmentSubject" type="text" placeholder="วิชา"/>
+							<FormControl
+								type="text"
+								placeholder="วิชา"
+								value={this.state.subject}
+								onChange={(e) => this.setState({subject: e.target.value})}/>
 						</Col>
 						<Col xs={3}>
-							<FormControl controlId="formAddAssignmentDue" type="text" placeholder="กำหนดส่ง"/>
+							<FormControl
+								type="text"
+								placeholder="กำหนดส่ง"
+								value={this.state.dueDate}
+								onChange={(e) => this.setState({dueDate: e.target.value})}/>
 						</Col>
 					</FormGroup>
-					<FormGroup controlId="formAddAssignmentBody">
+					<FormGroup>
 						<Col xs={10}>
-							<FormControl controlId="formAddAssignmentDescription" type="text" placeholder="คำอธิบาย"/>
+							<FormControl
+								type="text"
+								placeholder="คำอธิบาย"
+								value={this.state.description}
+								onChange={(e) => this.setState({description: e.target.value})}/>
 						</Col>
 					</FormGroup>
-					<Button type="submit">Submit</Button>
+					<Button type="submit" onClick={this.submitProcess}>Submit</Button>
 				</form>
 			</div>
 		)
